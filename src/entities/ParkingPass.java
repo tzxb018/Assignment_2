52caa5bb59e6dd249f78bb0c6b103a021742d462
Tomo <tzxbapp018@gmail.com>
2018-09-20 12:28:18 -0500

    Parking Pass

--- src/entities/ParkingPass.java
+++ src/entities/ParkingPass.java
@@ -2,4 +2,23 @@ package entities;
 
 public class ParkingPass extends Product {
 
+	private double cost;
+	
+	//constructor using the super class (product)
+	public ParkingPass(String code, double cost) {
+		super(code);
+		this.cost = cost;
+	}
+	
+	//getter and setter methods
+	public double getCost() {
+		return cost;
+	}
+
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+	
+	
+	
 }
