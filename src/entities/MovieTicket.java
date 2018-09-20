cb243e282029f69821f6c077e4af50b1b3b5914a
Tomo <tzxbapp018@gmail.com>
2018-09-20 11:56:50 -0500

    Movie Ticket subclass and product superclass

--- src/entities/MovieTicket.java
+++ src/entities/MovieTicket.java
@@ -1,5 +1,69 @@
 package entities;
 
-public class MovieTicket extends Product{
+import java.util.Date;
 
+public class MovieTicket extends Product{
+	
+	private Date movieDate;
+	private String movieName;
+	private Address location;
+	private String theatreLoc;
+	private double pricePerUnit;
+	
+	//constructor taking in private attributes and superclass attributes
+	public MovieTicket(String code, Date movieDate, String movieName, Address location, String theatreLoc,
+			double pricePerUnit) {
+		super(code);
+		this.movieDate = movieDate;
+		this.movieName = movieName;
+		this.location = location;
+		this.theatreLoc = theatreLoc;
+		this.pricePerUnit = pricePerUnit;
+	}
+
+	//getter and setter methods for all
+	public Date getMovieDate() {
+		return movieDate;
+	}
+
+	public void setMovieDate(Date movieDate) {
+		this.movieDate = movieDate;
+	}
+
+	public String getMovieName() {
+		return movieName;
+	}
+
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+
+	public Address getLocation() {
+		return location;
+	}
+
+	public void setLocation(Address location) {
+		this.location = location;
+	}
+
+	public String getTheatreLoc() {
+		return theatreLoc;
+	}
+
+	public void setTheatreLoc(String theatreLoc) {
+		this.theatreLoc = theatreLoc;
+	}
+
+	public double getPricePerUnit() {
+		return pricePerUnit;
+	}
+
+	public void setPricePerUnit(double pricePerUnit) {
+		this.pricePerUnit = pricePerUnit;
+	}
+
+	
+	
+	
+	
 }
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -1,5 +1,21 @@
 package entities;
 
 public class Product {
-
+	
+	private String code;
+	
+	//superclass constructor
+	public Product(String code) {
+		super();
+		this.code = code;
+	}
+	
+	//gettter and setter methods for superclass (shared with each subclass)
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
 }
