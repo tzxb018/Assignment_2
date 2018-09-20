f826af2ade67dd3678e55ecb90444aeb10c176ed
Tomo <tzxbapp018@gmail.com>
2018-09-20 12:33:08 -0500

    refershments and season pass

--- src/entities/Refreshments.java
+++ src/entities/Refreshments.java
@@ -1,5 +1,35 @@
 package entities;
 
 public class Refreshments extends Product {
+	
+	private String name;
+	private double cost;
+	
+	//creating construcotr superclass 
+	public Refreshments(String code, String name, double cost) {
+		super(code);
+		this.name = name;
+		this.cost = cost;
+	}
 
+	//getter and setter methods
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getCost() {
+		return cost;
+	}
+
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+	
+	
+	
+	
 }
--- src/entities/SeasonPass.java
+++ src/entities/SeasonPass.java
@@ -1,5 +1,56 @@
 package entities;
+import java.util.*;
 
 public class SeasonPass extends Product {
+	
+	private String name;
+	private Date start;
+	private Date end;
+	private double cost;
+	
+	//cosntructor using super class and private attributes
+	public SeasonPass(String code, String name, Date start, Date end, double cost) {
+		super(code);
+		this.name = name;
+		this.start = start;
+		this.end = end;
+		this.cost = cost;
+	}
 
+	//getter and setter methods
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getStart() {
+		return start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	public Date getEnd() {
+		return end;
+	}
+
+	public void setEnd(Date end) {
+		this.end = end;
+	}
+
+	public double getCost() {
+		return cost;
+	}
+
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+	
+	
+	
+	
 }
